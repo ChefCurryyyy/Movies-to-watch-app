1e3dbf56e7e01a045b9e39d5e04b52600f42be57
Ahmad <ahmad08ali@gmail.com>
2025-12-09 13:48:55 +0100

    create backend and frontend prjs + backend models

---
+++ backend/Models/Movie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MoviesToWatchApp.Backend.Models;
+
+public class Movie
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public Priority Priority { get; set; } = Priority.Medium;
+    public Status Status { get; set; } = Status.ToWatch;
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+        
+    public enum Priority
+    {
+        High = 1,
+        Medium = 2,
+        Low = 3
+    }
+
+    public enum Status
+    {
+        ToWatch = 1,
+        Watching = 2,
+        Seen = 3
+    }
+}
